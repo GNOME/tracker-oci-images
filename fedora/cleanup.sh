a4d30df4efa1c5dc0febaeabb8f2fe7849396fa6
Sam Thursfield <sam@afuera.me.uk>
2019-05-26 19:26:03 +0200

    Remove Tracker package from test environment
    This is possible from Tracker 2.3 onwards, due to
    https://gitlab.gnome.org/GNOME/tracker/merge_requests/95.

--- fedora/cleanup.sh
+++ fedora/cleanup.sh
@@ -2,6 +2,12 @@
 
 set -eu
 
+# This is installed by `dnf build-dep tracker-miners`. We don't want it in the
+# test environment, so we can be sure that tests are running against what's in
+# the build tree and not what's installed in /usr.
+echo "Removing Tracker package"
+dnf remove -y tracker
+
 echo "Removing DNF cache"
 dnf clean all
 
