15a622e40384a912af56a0dae8960c648ef16ff3
Sam Thursfield <sam@afuera.me.uk>
2019-12-13 23:36:14 +0100

    Create 'tracker' user in the Ubuntu base image

--- debian/prepare.sh
+++ debian/prepare.sh
@@ -9,3 +9,6 @@ apt-get -yq build-dep tracker tracker-miners
 
 # For rss miner, which I guess Debian/Ubuntu disable by default
 apt-get -yq install libgrss-dev
+
+# We need to be able to run tests as a normal user, not 'root'.
+useradd -Um tracker
