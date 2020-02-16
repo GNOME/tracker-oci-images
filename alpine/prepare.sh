657347a3a784877a037342cf9ae11d82da2292fc
Sam Thursfield <sam@afuera.me.uk>
2020-02-17 00:10:20 +0100

    Add eatmydata to CI images
    See https://gitlab.gnome.org/GNOME/tracker/merge_requests/176 for
    rationale.

--- alpine/prepare.sh
+++ alpine/prepare.sh
@@ -7,3 +7,21 @@ apk add -U alpine-sdk bash-completion dbus-dev git glib-dev gobject-introspectio
 
 addgroup -S tracker
 adduser -D -G tracker -g tracker tracker
+
+
+# Build eatmydata from Git.
+#
+# This is to speed up the tests. See:
+# https://gitlab.gnome.org/GNOME/tracker/merge_requests/176
+#
+# Currently https://gitlab.gnome.org/GNOME/tracker-oci-images/merge_requests/13
+# is needed to build against musl libc.
+apk add -U autoconf automake libtool
+git clone https://github.com/ssssam/libeatmydata --branch sam/musl-open64-fix
+cd libeatmydata
+autoreconf -ivf
+./configure
+make
+make install
+cd ..
+apk del autoconf automake libtool
