0b4788e476e448363fb7836258c3abac3e2950b5
Sam Thursfield <sam@afuera.me.uk>
2020-11-23 20:12:58 +0100

    alpine: Add dependencies needed to build Tracker Miners
    Taken from https://gitlab.gnome.org/GNOME/tracker-miners/-/merge_requests/137
    many thanks to Cogitri.

--- alpine/prepare.sh
+++ alpine/prepare.sh
@@ -3,9 +3,17 @@
 set -ex
 
 echo 'https://alpine.global.ssl.fastly.net/alpine/edge/community/' >> /etc/apk/repositories
+
+# Tracker SPARQL dependencies
 apk add -U alpine-sdk asciidoc bash-completion dbus-dev git glib-dev gobject-introspection-dev \
            gtk-doc icu-dev json-glib-dev libsoup-dev libxml2-dev meson networkmanager-dev \
            py3-gobject3 py3-setuptools sqlite-dev vala
+
+# Tracker Miners dependencies
+apk add gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly
+        gstreamer-dev libcue-dev libgrss-dev libgsf-dev libgxps-dev
+        libosinfo-dev libseccomp-dev poppler-dev shared-mime-info tiff-dev \
+        totem-pl-parser-dev upower-dev
 
 addgroup -S tracker
 adduser -D -G tracker -g tracker tracker
